d66581431f6e93ec350126ac1305025570de94e3
MeowMeow2299 <vht09032207@gmail.com>
2025-09-09 17:57:34 +0700

    Fix image paths - use existing JL images instead of non-existent JL000-JL015

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,22 +3,22 @@ import "./Slider.css";
 
 const Slider = () => {
   const items = [
-    { id: 1, title: "Super Ace", provider: "Jili", img: "/photo/JL000.png", isFavorite: false },
-    { id: 2, title: "Fortune Gems", provider: "Jili", img: "/photo/JL001.png", isFavorite: true },
-    { id: 3, title: "Aviator", provider: "Spribe", img: "/photo/JL002.png", isFavorite: false },
-    { id: 4, title: "Super Ace Deluxe", provider: "Jili", img: "/photo/JL003.png", isFavorite: true },
-    { id: 5, title: "Money Coming", provider: "Jili", img: "/photo/JL004.png", isFavorite: false },
-    { id: 6, title: "Boxing King", provider: "Jili", img: "/photo/JL005.png", isFavorite: true },
-    { id: 7, title: "Wild Bounty Showdown", provider: "Pocket Games Soft", img: "/photo/JL006.png", isFavorite: false },
-    { id: 8, title: "Golden Empire", provider: "Jili", img: "/photo/JL007.png", isFavorite: true },
-    { id: 9, title: "777-Classic Style", provider: "BTGaming", img: "/photo/JL008.png", isFavorite: false },
-    { id: 10, title: "Happy Fishing", provider: "Jili", img: "/photo/JL009.png", isFavorite: true },
-    { id: 11, title: "Party Star", provider: "Jili", img: "/photo/JL010.png", isFavorite: false },
-    { id: 12, title: "Leprechaun Riches", provider: "Pocket Games Soft", img: "/photo/JL011.png", isFavorite: true },
-    { id: 13, title: "Legacy of Egypt", provider: "Jili", img: "/photo/JL012.png", isFavorite: false },
-    { id: 14, title: "Wild Ace", provider: "Jili", img: "/photo/JL013.png", isFavorite: true },
-    { id: 15, title: "Hot Games", provider: "Jili", img: "/photo/JL014.png", isFavorite: false },
-    { id: 16, title: "Lucky Slots", provider: "Jili", img: "/photo/JL015.png", isFavorite: true },
+    { id: 1, title: "Super Ace", provider: "Jili", img: "/photo/superace.png", isFavorite: false },
+    { id: 2, title: "Fortune Gems", provider: "Jili", img: "/photo/JL0002.png", isFavorite: true },
+    { id: 3, title: "Aviator", provider: "Spribe", img: "/photo/JL0003.png", isFavorite: false },
+    { id: 4, title: "Super Ace Deluxe", provider: "Jili", img: "/photo/JL0004.png", isFavorite: true },
+    { id: 5, title: "Money Coming", provider: "Jili", img: "/photo/JL0005.png", isFavorite: false },
+    { id: 6, title: "Boxing King", provider: "Jili", img: "/photo/JL0006.png", isFavorite: true },
+    { id: 7, title: "Wild Bounty Showdown", provider: "Pocket Games Soft", img: "/photo/JL0007.png", isFavorite: false },
+    { id: 8, title: "Golden Empire", provider: "Jili", img: "/photo/JL0008.png", isFavorite: true },
+    { id: 9, title: "777-Classic Style", provider: "BTGaming", img: "/photo/JL0009.png", isFavorite: false },
+    { id: 10, title: "Happy Fishing", provider: "Jili", img: "/photo/JL0011.png", isFavorite: true },
+    { id: 11, title: "Party Star", provider: "Jili", img: "/photo/JL0021.png", isFavorite: false },
+    { id: 12, title: "Leprechaun Riches", provider: "Pocket Games Soft", img: "/photo/JL0029.png", isFavorite: true },
+    { id: 13, title: "Legacy of Egypt", provider: "Jili", img: "/photo/JL0036.png", isFavorite: false },
+    { id: 14, title: "Wild Ace", provider: "Jili", img: "/photo/JL0038.png", isFavorite: true },
+    { id: 15, title: "Hot Games", provider: "Jili", img: "/photo/JL0045.png", isFavorite: false },
+    { id: 16, title: "Lucky Slots", provider: "Jili", img: "/photo/JL0050.png", isFavorite: true },
   ];
 
   return (
